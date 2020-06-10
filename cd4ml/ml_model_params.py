a0a05e3c8b16d3eef088faf23eb6d9a7fe93916a
Thomas Ascioglu <thomasascioglu@gmail.com>
2020-06-10 12:55:38 +0200

    change random forest estimators to 100

--- cd4ml/ml_model_params.py
+++ cd4ml/ml_model_params.py
@@ -1,16 +1,6 @@
 model_parameters = {
-    "random_forest": {
-        "n_estimators": 10,
-        "max_features": 0.5
-    },
-    "adaboost": {
-        "n_estimators": 100
-    },
-    "gradient_boosting": {
-        "n_estimators": 200,
-        "max_depth": 4
-    },
-    "decision_tree": {
-        "criterion": 'mse'
-    }
+    "random_forest": {"n_estimators": 100, "max_features": 0.5},
+    "adaboost": {"n_estimators": 100},
+    "gradient_boosting": {"n_estimators": 200, "max_depth": 4},
+    "decision_tree": {"criterion": "mse"},
 }
